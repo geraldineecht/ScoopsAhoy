913043e5b4fda2f548d9bdf65e7fe9583ff44da4
EduardoHGz <88729982+LaloHGz@users.noreply.github.com>
2022-09-06 10:58:33 -0500

    storyboard de pantalla inicio
    Diseño responsivo para iphone 11 y SE de la pantalla de inicio

--- appCaritasMty/appCaritasMty/ViewController.swift
+++ appCaritasMty/appCaritasMty/ViewController.swift
@@ -2,18 +2,32 @@
 //  ViewController.swift
 //  appCaritasMty
 //
-//  Created by Eduardo Jair Hernández Gómez on 06/09/22.
+//  Created on 06/09/22.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewInfo: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        viewInfo.roundCorners(corners: [.topLeft, .topRight], radius: 30)
+    }
 
 }
 
+extension UIView {
+   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
