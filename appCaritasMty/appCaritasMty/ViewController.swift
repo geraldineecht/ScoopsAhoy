63335964425a8e12096a772cb241d8288415f596
EduardoHGz <88729982+LaloHGz@users.noreply.github.com>
2022-09-06 09:55:08 -0500

    proyecto creado

---
+++ appCaritasMty/appCaritasMty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appCaritasMty
+//
+//  Created by Eduardo Jair Hernández Gómez on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
