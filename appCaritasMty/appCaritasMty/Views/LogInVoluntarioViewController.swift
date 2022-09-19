d2ee09e5024dd695bf59b38831b2ac3857acd8f0
geraldineecht <geraldineecht@gmail.com>
2022-09-19 00:04:49 -0500

    UI Log In Voluntario

---
+++ appCaritasMty/appCaritasMty/Views/LogInVoluntarioViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInVoluntarioViewController.swift
+//  appCaritasMty
+//
+//  Created by Geraldine Torres on 18/09/22.
+//
+
+import UIKit
+
+class LogInVoluntarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
