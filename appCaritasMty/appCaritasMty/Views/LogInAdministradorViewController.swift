e27f92e3db54366ce64360b5f8077c19a0fefe4b
geraldineecht <geraldineecht@gmail.com>
2022-09-19 16:28:15 -0500

    log in Admin

---
+++ appCaritasMty/appCaritasMty/Views/LogInAdministradorViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LogInAdministradorViewController.swift
+//  appCaritasMty
+//
+//  Created by Geraldine Torres on 19/09/22.
+//
+
+import UIKit
+
+class LogInAdministradorViewController: UIViewController {
+    
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tfEmail.layer.cornerRadius = 5
+        tfEmail.layer.masksToBounds = true
+        tfEmail.layer.borderWidth = 1
+        tfEmail.layer.borderColor = UIColor(rgb: 0x0099A9).cgColor
+        tfEmail.backgroundColor = UIColor(rgb: 0x4397A7).withAlphaComponent(0.08)
+        tfEmail.setLeftPaddingPoints(10)
+        tfEmail.setRightPaddingPoints(10)
+        tfPassword.layer.cornerRadius = 5
+        tfPassword.layer.masksToBounds = true
+        tfPassword.layer.borderWidth = 1
+        tfPassword.layer.borderColor = UIColor(rgb: 0x0099A9).cgColor
+        tfPassword.backgroundColor = UIColor(rgb: 0x4397A7).withAlphaComponent(0.08)
+        tfPassword.setLeftPaddingPoints(10)
+        tfPassword.setRightPaddingPoints(10)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
