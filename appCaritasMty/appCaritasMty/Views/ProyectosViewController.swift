a676cfc3bb6b9f02405d3384de1d5238f6f65bcf
cristina-hdzp <A00830775@tec.mx>
2022-10-01 16:40:34 -0500

    Avance de FrontEnd
    - Se realizo avance de pantalla de Proyectos y Detalles
    - Pendiente style de componentes

---
+++ appCaritasMty/appCaritasMty/Views/ProyectosViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProyectosViewController.swift
+//  appCaritasMty
+//
+//  Created by Alumno on 01/10/22.
+//
+
+import UIKit
+
+class ProyectosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // CardView.layer.cornerRadius = 5
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
