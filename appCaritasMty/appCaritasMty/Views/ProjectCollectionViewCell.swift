2ed3b654e9f2e48244eefb48b7e8a90a9594cab7
Carmina López <a00830265@itesm.mx>
2022-10-06 07:35:55 -0500

    Collection view
    Se agregó el backend de collection view y se esta haciendo el segue de la vista de detalle (en proceso).

--- appCaritasMty/appCaritasMty/Views/ProjectCollectionViewCell.swift
+++ appCaritasMty/appCaritasMty/Views/ProjectCollectionViewCell.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class ProjectCollectionViewCell: UICollectionViewCell {
+    static let identifier = "projectCell"
     
     @IBOutlet weak var lbProjectName: UILabel!
     @IBOutlet weak var lbProjectDesc: UILabel!
     @IBOutlet weak var imgProjectImg: UIImageView!
     
     @IBOutlet weak var btnDetailView: UIButton!
-    static let identifier = "projectCell"
     
-    
+    func setupCell(name: String, desc: String, image: UIImage){
+        imgProjectImg.image = image
+        lbProjectDesc.text = desc
+        lbProjectName.text = name
+        imgProjectImg.layer.cornerRadius = 5
+    }
     
 }
